56670ffca58f2f1d6771e1a057af8e817d5d863e
Francesco De Lisi <francesco@intelligenttrading.org>
2019-03-26 11:03:40 +0700

    Add SSL

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const path = require("path");
-var express = require("express");
-var serveStatic = require("serve-static");
-const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-var port = process.env.PORT || 5000;
-app.listen(port);
+const path = require('path')
+var express = require('express')
+var serveStatic = require('serve-static')
+const enforce = require('express-sslify')
+const app = express()
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(serveStatic(path.join(__dirname, 'dist')))
+var port = process.env.PORT || 5000
+app.listen(port)
